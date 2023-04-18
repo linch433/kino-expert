3a689f7ae31eb38db3ecc066dc8a966d66d37692
Volodymyr Kopynets <linch433@gmail.com>
2023-04-18 14:23:38 +0300

    Fix layout on FilmContentPage

--- src/components/Content/FilmPage/FilmCard.jsx
+++ src/components/Content/FilmPage/FilmCard.jsx
@@ -17,7 +17,7 @@ const FilmCard = ({ currentFilm }) => {
     trailer_url,
   } = currentFilm;
   const embedUrl = `https://www.youtube.com/embed/${StringServices.getEmbedString(
-    trailer_url
+    trailer_url,
   )}`;
   const videoDetails = [
     { label: 'Duration:', value: `${duration} minutes` },
@@ -41,50 +41,49 @@ const FilmCard = ({ currentFilm }) => {
   };
 
   return (
-    <div className="relative py-10 mx-5 flex flex-wrap items-center bg-int-gray-secondary text-int-white-main rounded-lg mb-5">
-      <div className="w-full md:w-[45%] lg:w-[38%] ml-auto mr-auto px-2">
+    <div
+      className='p-5 mx-5 flex flex-col justify-center items-center xl:flex-row xl:items-start bg-int-gray-secondary text-int-white-main rounded-lg mb-5 gap-8'>
+      <div className='w-full md:w-[45%] lg:w-[38%] px-2'>
         <img
           src={image_url}
-          alt="poster"
-          className="max-w-full rounded-lg shadow-lg"
+          alt='poster'
+          className='max-w-full rounded-lg shadow-lg'
         />
       </div>
-      <div className="w-full md:w-6/12 ml-auto mr-auto px-2 sm:mt-2">
-        <div className="md:pr-12">
-          <div className="pt-3 text-3xl font-semibold">{name}</div>
-          <div className="mt-4 text-lg leading-relaxed">{description}</div>
+      <div className='w-full md:w-7/12 px-2'>
+        <div className='text-3xl font-semibold'>{name}</div>
+        <div className='mt-4 text-lg leading-relaxed'>{description}</div>
 
-          {videoDetails.map((detail, index) => (
-            <div key={index} className="mt-2 text-lg leading-relaxed">
-              <span className="font-semibold">{detail.label} </span>
-              {detail.value}
-            </div>
-          ))}
+        {videoDetails.map((detail, index) => (
+          <div key={index} className='mt-2 text-lg leading-relaxed'>
+            <span className='font-semibold'>{detail.label} </span>
+            {detail.value}
+          </div>
+        ))}
 
-          <div
-            className={`mt-2 text-lg flex flex-col leading-relaxed justify-center items-center rounded-lg py-3 bg-int-gray-main hover:bg-input font-semibold cursor-pointer
+        <div
+          className={`mt-2 text-lg flex flex-col leading-relaxed justify-center items-center rounded-lg py-3 bg-int-gray-main hover:bg-input font-semibold cursor-pointer
             ${isAuth ? '' : 'hidden'}`}
-            onClick={toggleModalOpen}
-          >
-            Add review
-          </div>
+          onClick={toggleModalOpen}
+        >
+          Add review
+        </div>
 
-          {isModalOpen && <FilmReviewForm toggleModal={toggleModalClose} />}
-          <Link
-            to={trailer_url}
-            className="mt-2 text-lg flex flex-col leading-relaxed justify-center items-center rounded-lg py-3 bg-int-gray-main hover:bg-input font-semibold lg:hidden"
-            target="_blank"
-          >
-            Watch trailer
-          </Link>
-          <div className="mt-2 items-center hidden justify-center w-full lg:flex lg:h-[29.5rem]">
-            <iframe
-              src={embedUrl}
-              className="w-full h-full"
-              title="Trailer"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            ></iframe>
-          </div>
+        {isModalOpen && <FilmReviewForm toggleModal={toggleModalClose} />}
+        <Link
+          to={trailer_url}
+          className='mt-2 text-lg flex flex-col leading-relaxed justify-center items-center rounded-lg py-3 bg-int-gray-main hover:bg-input font-semibold lg:hidden'
+          target='_blank'
+        >
+          Watch trailer
+        </Link>
+        <div className='mt-2 items-center hidden justify-center w-full xl:flex h-[29.5rem]'>
+          <iframe
+            src={embedUrl}
+            className='w-full h-full'
+            title='Trailer'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          ></iframe>
         </div>
       </div>
     </div>
